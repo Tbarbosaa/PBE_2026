6da79fb3fef6fd70d7db5496a4b6c5f747ff1613
TOBIAS BARBOSA MOREIRA <tobias.moreira@portalsesisp.org.br>
2026-07-29 11:06:36 -0300

    primeiro commit

---
+++ 01Aula/02atividade.php
@@ -0,0 +1,19 @@
+<?php
+
+//Declaração de variável
+$nome = "EStudante";
+$idade = 20;
+
+//Exibindo Informações
+echo "Olá, $nome!";
+echo "Você tem $idade anos.";
+
+// 
+if ($idade >= 18){
+    echo "Você é maior de idade";
+}else {
+    echo "Você é menor de idade";
+}
+
+
+?>
